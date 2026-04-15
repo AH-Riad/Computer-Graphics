da7bd77e517c277d358e4c066b7892578ff7b21c
Riad <riadk910@gmail.com>
2026-04-15 11:55:04 +0600

    DDA Algorithm

---
+++ DDA.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+
+# starting and ending points
+x1, y1 = 2, 3
+x2, y2 = 10, 8
+
+dx = x2 - x1
+dy = y2 - y1
+
+steps = max(abs(dx), abs(dy))
+
+x_inc = dx / steps
+y_inc = dy / steps
+
+x = x1
+y = y1
+
+x_points = []
+y_points = []
+
+print("Step\tX\tY\tRounded X\tRounded Y")
+
+# simple loop (no underscore)
+for i in range(steps + 1):
+    xr = round(x)
+    yr = round(y)
+
+    print(i, "\t", f"{x:.2f}", "\t", f"{y:.2f}", "\t", xr, "\t\t", yr)
+
+    x_points.append(xr)
+    y_points.append(yr)
+
+    x = x + x_inc
+    y = y + y_inc
+
+
+# draw the line
+plt.plot(x_points, y_points, marker='o')
+plt.xlabel("X-axis")
+plt.ylabel("Y-axis")
+plt.title("DDA Line Drawing Algorithm")
+plt.grid()
+plt.show()
