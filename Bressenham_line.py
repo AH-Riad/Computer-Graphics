aedde975739b7cfe92817522790e48b9ad7216e5
Riad <riadk910@gmail.com>
2026-04-15 15:22:27 +0600

    Bressenham Line Drawing

---
+++ Bressenham_line.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+
+x1, y1 = 2, 3
+x2, y2 = 10, 8
+
+x = x1
+y = y1
+
+dx = x2 - x1
+dy = y2 - y1
+
+p = 2 * dy - dx   # decision parameter
+
+x_points = []
+y_points = []
+
+print("Step\tX\tY\tp")
+
+for i in range(dx + 1):
+    print(i, "\t", x, "\t", y, "\t", p)
+
+    x_points.append(x)
+    y_points.append(y)
+
+    x = x + 1   # always move in x direction
+
+    if p < 0:
+        p = p + 2 * dy
+    else:
+        y = y + 1
+        p = p + 2 * dy - 2 * dx
+
+
+plt.plot(x_points, y_points, marker='o')
+plt.title("Bresenham Line Drawing Algorithm")
+plt.grid()
+plt.show()
