0bb88a2efd5f8deb0e73f78f0ea685509f6bd47f
Riad <riadk910@gmail.com>
2026-04-15 16:32:56 +0600

    Midpoint circle

---
+++ mid_point_circle.py
@@ -0,0 +1,41 @@
+import matplotlib.pyplot as plt
+
+xc, yc = 0, 0
+r = 5
+
+x = 0
+y = r
+
+p = 1 - r
+
+x_points = []
+y_points = []
+
+def draw_circle(xc, yc, x, y):
+    x_points.extend([xc+x, xc-x, xc+x, xc-x, xc+y, xc-y, xc+y, xc-y])
+    y_points.extend([yc+y, yc+y, yc-y, yc-y, yc+x, yc+x, yc-x, yc-x])
+
+
+print("Step\tX\tY\tp")
+
+step = 0
+while x <= y:
+    print(step, "\t", x, "\t", y, "\t", p)
+
+    draw_circle(xc, yc, x, y)
+
+    if p < 0:
+        p = p + 2 * x + 3
+    else:
+        p = p + 2 * (x - y) + 5
+        y = y - 1
+
+    x = x + 1
+    step += 1
+
+
+plt.scatter(x_points, y_points)
+plt.gca().set_aspect('equal', adjustable='box')
+plt.title("Midpoint Circle Drawing Algorithm")
+plt.grid()
+plt.show()
